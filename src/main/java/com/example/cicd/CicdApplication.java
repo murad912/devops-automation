1e8f62f65bd1f9a1b54b302c3964c05452469883
murad912 <49727792+murad912@users.noreply.github.com>
2024-07-02 01:30:57 -0500

    get Api

--- src/main/java/com/example/cicd/CicdApplication.java
+++ src/main/java/com/example/cicd/CicdApplication.java
@@ -2,9 +2,15 @@ package com.example.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CicdApplication {
+
+	@GetMapping
+	public String message(){
+        return "Hello World";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
